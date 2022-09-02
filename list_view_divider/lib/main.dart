bc63a09affaef2bbba93b9a81a84ecd120b048a6
arsikz3 <arsen.ziyadinov@gmail.com>
2022-09-03 00:22:14 +0300

    first commit

---
+++ list_view_divider/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var listData = List.generate(50, (index) => index);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: ListView.separated(
+        itemCount: listData.length,
+        itemBuilder: (context, position) {
+          return Text(position.toString());
+        },
+        separatorBuilder: (context, position) {
+          return Divider();
+        },
+      ),
+    ));
+  }
+}
