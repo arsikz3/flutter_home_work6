d7955226d5a9a0b81a812adabd47ec3884f39e0e
arsikz3 <arsen.ziyadinov@gmail.com>
2022-09-04 00:11:04 +0300

    Задание 6.1 safearea

--- horizontal_list/lib/main.dart
+++ horizontal_list/lib/main.dart
@@ -19,24 +19,71 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
+  MyHomePage({Key? key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var list = List<int>.generate(10, (index) => index + 1);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Horizontal list'),
+        title: Text('List horizont'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
-        ),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          double width = constraints.constrainWidth();
+
+          print(width);
+
+          return width < 500
+              ? /*Padding(
+                  padding: EdgeInsets.only(top: 100),
+                  child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: List.generate(
+                            list.length,
+                            (index) => Padding(
+                                  padding: EdgeInsets.only(right: 10),
+                                  child: Container(
+                                      height: 100,
+                                      width: 100,
+                                      color: Colors.green[index * 100]),
+                                )),
+                      )),
+                )*/
+              GridView.builder(
+                  scrollDirection: Axis.horizontal,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: width < 500 ? 1 : 3,
+                    childAspectRatio: width < 500 ? 10 : 1,
+                  ),
+                  itemCount: list.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      height: 80,
+                      width: 80,
+                      color: Colors.green[index * 100],
+                    );
+                  })
+              : ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: list.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: EdgeInsets.only(top: 10),
+                      child: Container(
+                        height: 80,
+                        width: 80,
+                        color: Colors.green[index * 100],
+                      ),
+                    );
+                  });
+        },
       ),
     );
   }
