7afa8bd732f18cc47fab12dd578b177cdf0b3f71
arsikz3 <arsen.ziyadinov@gmail.com>
2022-09-03 23:31:17 +0300

    задание 6.1 safearea

--- list_view_divider/lib/main.dart
+++ list_view_divider/lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo 6.1',
+      title: 'Flutter Demo 6.1.',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
