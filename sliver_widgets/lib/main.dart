2b5137c1fecdf4d45fb6d9242692f0f98894d663
arsikz3 <arsen.ziyadinov@gmail.com>
2022-09-04 13:52:39 +0300

    Задание 6.3

--- sliver_widgets/lib/main.dart
+++ sliver_widgets/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
+  MyHomePage({Key? key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -29,16 +29,39 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Sliver widgets',
+        drawer: Drawer(),
+        body: SafeArea(
+          child: CustomScrollView(slivers: <Widget>[
+            //2
+            SliverAppBar(
+              //forceElevated: true,
+              //primary: true,
+              //excludeHeaderSemantics: true,
+              //floating: true,
+              //stretch: true,
+              //snap: true,
+              pinned: true,
+              expandedHeight: 250.0,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text('Simple text', textScaleFactor: 1),
+                background: Image.asset('assets/images/istockphoto.jpg',
+                    fit: BoxFit.fill, color: Colors.black.withOpacity(1.0)),
+              ),
             ),
-          ],
-        ),
-      ),
-    );
+            SliverList(delegate: SliverChildListDelegate([Text('radomWords')])),
+            SliverFixedExtentList(
+              itemExtent: 50,
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return Container(
+                    alignment: Alignment.center,
+                    color: Colors.green,
+                    child: Text("Index n°$index"),
+                  );
+                },
+              ),
+            )
+          ]),
+        ));
   }
 }
